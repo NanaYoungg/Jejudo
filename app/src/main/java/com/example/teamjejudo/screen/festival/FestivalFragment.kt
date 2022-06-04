0e6ecc844d88dbfef8267b876fbcb9e7843846ad
Nayoung <dudtnsl1004@gmail.com>
2022-06-04 16:51:45 +0900

    update recyclerView

--- app/src/main/java/com/example/teamjejudo/screen/festival/FestivalFragment.kt
+++ app/src/main/java/com/example/teamjejudo/screen/festival/FestivalFragment.kt
@@ -1,30 +1,24 @@
 package com.example.teamjejudo.screen.festival
 
-import android.hardware.lights.LightsManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.teamjejudo.R
 import com.example.teamjejudo.adapter.FestivalAdapter
 import com.example.teamjejudo.data.Festival
 import com.example.teamjejudo.databinding.FragmentFestivalBinding
-import timber.log.Timber
-import java.util.ArrayList
 
 class FestivalFragment : Fragment() {
 
     private var _binding: FragmentFestivalBinding? = null
-//    private var festival = ArrayList<Festival>()
-
     private val binding get() = _binding!!
 
     private lateinit var adapter: FestivalAdapter
-    val mDatas = mutableListOf<Festival>()
+    private val festival = mutableListOf<Festival>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,35 +33,26 @@ class FestivalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        initFestivalAdapter()
-        initializelist()
-        initDogRecyclerView()
+        initFestivalAdapter()
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
-    //
-//    private fun initFestivalAdapter() {
-//        binding.rvFestival.adapter = FestivalAdapter(festival, requireContext())
-//        binding.rvFestival.layoutManager =
-//            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-//        Timber.d("sdfsdfsdfsdfsdfsdf")
-//    }
-    fun initDogRecyclerView() {
+    private fun initFestivalAdapter() {
         val adapter = FestivalAdapter() //어댑터 객체 만듦
-        adapter.datalist = mDatas //데이터 넣어줌
+        adapter.festivalList = festival //데이터 넣어줌
         binding.rvFestival.adapter = adapter //리사이클러뷰에 어댑터 연결
-        binding.rvFestival.layoutManager = LinearLayoutManager(context) //레이아웃 매니저 연결
+        binding.rvFestival.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false) //레이아웃 매니저 연결
     }
 
-    fun initializelist() { //임의로 데이터 넣어서 만들어봄
-        with(mDatas) {
-            add(Festival("test1", 1, "1", "2"))
-            add(Festival("test2", 2, "20", "M"))
-            add(Festival("test3", 3, "20", "M"))
-        }
-    }
+//    fun initializelist() { //임의로 데이터 넣어서 만들어봄
+//        with(festival) {
+//            add(Festival("test1", 1, "1", "2"))
+//            add(Festival("test2", 2, "20", "M"))
+//            add(Festival("test3", 3, "20", "M"))
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
